9e052f6fcc8eb75032bde70877a80f73358db726
Jamil Shamar <gamilaliqaid.shamar@xchanging.com>
2019-10-27 03:48:54 +0800

    load json file

--- Contacts/AppDelegate.swift
+++ Contacts/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+          self.window?.rootViewController?.navigate(type: .root, module: GeneralRoute.contacts)
         return true
     }
 
--- Contacts/Modules/Contacts/ContactsViewController.swift
+++ Contacts/Modules/Contacts/ContactsViewController.swift
@@ -7,7 +7,14 @@
 
 
 import UIKit
+import SwiftyJSON
 
+enum FileLoadError: Error {
+    case failedLoadingResource(String)
+    case failedSerialization
+    case failedRightNavBarImage
+    
+}
 protocol IContactsViewController: class {
 	var router: IContactsRouter? { get set }
 }
@@ -15,11 +22,59 @@ protocol IContactsViewController: class {
 class ContactsViewController: UIViewController {
 	var interactor: IContactsInteractor?
 	var router: IContactsRouter?
+    fileprivate var defaults = UserDefaults.standard
+    fileprivate var jsonData: Data?
 
 	override func viewDidLoad() {
         super.viewDidLoad()
 		// do someting...
+        
+        do {
+            // load json file
+            
+            try loadJsonFile(fileName: "data")
+            
+            
+        } catch  {
+            
+        }
     }
+    
+    
+    func loadJsonFile (fileName:String = "data", extensionfile : String = "json") throws -> Bool  {
+        
+        if defaults.bool(forKey: "fileLoaded") == false {
+            
+            
+            
+            if let file = Bundle.main.path(forResource: fileName, ofType: extensionfile) {
+                jsonData = try? Data(contentsOf: URL(fileURLWithPath: file))
+            } else {
+                throw FileLoadError.failedLoadingResource(fileName)
+            }
+            
+            
+            if let jsonObject = try? JSONSerialization.jsonObject(with: jsonData!, options: .allowFragments) {
+                
+                let contact : Array =  JSON(jsonObject).arrayValue
+                // saving data to local db
+                
+                
+                defaults.set(true, forKey: "fileLoaded")
+                
+                
+            } else {
+                throw FileLoadError.failedSerialization
+                
+            }
+            print(defaults.dictionaryRepresentation().values)
+            
+            
+        }
+        
+        return true
+    }
+    
 }
 
 extension ContactsViewController: IContactsViewController {
