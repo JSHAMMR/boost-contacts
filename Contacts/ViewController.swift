75276fca3fd75ce6fc878346a4b34360ce99b2e2
Jamil Shamar <gamilaliqaid.shamar@xchanging.com>
2019-10-27 00:37:13 +0800

    initial commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Gamil Ali Qaid Shamar on 27/10/2019.
+//  Copyright © 2019 Jamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
