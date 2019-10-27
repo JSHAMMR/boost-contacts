d1880c96c908f84fe4ded467a40b999619674148
Jamil Shamar <gamilaliqaid.shamar@xchanging.com>
2019-10-27 22:24:14 +0800

    Unit Test Contact VC

--- ContactsTests/ContactsTests.swift
+++ ContactsTests/ContactsTests.swift
@@ -5,30 +5,45 @@
 //  Created by Gamil Ali Qaid Shamar on 27/10/2019.
 //  Copyright © 2019 Jamil. All rights reserved.
 //
-
 import XCTest
+import RealmSwift
 @testable import Contacts
 
 class ContactsTests: XCTestCase {
-
+    
+    var contact : ContactsViewController!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        contact = ContactsViewController ()
+        _ = contact.view
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    
+    func testInitTableView() {
+        XCTAssertNotNil(contact.tableView)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testLoadingResource() {
+        XCTAssertTrue(try contact.loadJsonFile())
+        XCTAssertTrue(try contact.loadJsonFile(fileName: "data", extensionfile: "json"))
     }
-
+    
+    func testRightNavigationItem() {
+        XCTAssertTrue(try contact.setRightNavigationBarItem(imageName: "accordion-plus"))
+        
+    }
+    //
+    func testRegisterCell() {
+        XCTAssertTrue(try contact.registerNibCell(nibNameString: "ContactCell", reuseIdentifier: "ContactCell"))
+        
+    }
+    
+    
 }
